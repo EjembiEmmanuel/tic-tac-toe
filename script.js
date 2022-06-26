d99441145a61a97784b05513e236a2638f42680a
Ejembi Emmanuel <ejembioche50@gmail.com>
2022-06-26 19:00:46 +0100

    Created function to handle player turns

--- script.js
+++ script.js
@@ -15,6 +15,30 @@ const Player = (name, letter) => {
 }
 
 
+const GetPlayer = (player1, player2) => {
+    player1 = Player("Oche", "X")
+    player2 = Player("Ben", "O")
+
+    return {player1, player2}
+}
+
+
+let currentPlayer
+let played = false
+const GetPlayerTurn = (player1, player2) => {
+    
+    if(player1.letter === "X" && !played) {
+        currentPlayer = player1
+        played = true
+    } else {
+        currentPlayer = player2
+        played = false
+    }
+
+    return currentPlayer
+}
+
+
 const Gameboard = (function() {
     let gameboard = [
             
@@ -26,7 +50,9 @@ const Gameboard = (function() {
 }())
 
 
+
 const DisplayController = (function(doc, board) {
+
 
     const getElement = () => {
         let element = doc.querySelectorAll(".board-item")
@@ -39,7 +65,8 @@ const DisplayController = (function(doc, board) {
     }
 
     const displayContent = (selectors, content) => {
-        const oche = Player("Oche", "X")
+    
+        const {player1, player2} = GetPlayer()
 
         content = getContent()
         selectors = getElement()
@@ -47,12 +74,19 @@ const DisplayController = (function(doc, board) {
         selectors.forEach(btn => btn.addEventListener('click', function() {
             const index = Array.from(selectors).indexOf(btn);
 
+            let player = GetPlayerTurn(player1, player2)
+            console.log(player)
+
             if(btn.innerText === "") {
-                btn.innerText = oche.letter
+                btn.innerText = player.letter
                 content.push(btn.innerText)
+
+                gameOver()
             }
         }));
     }
+
+    
 
     const displayBoard = () => {
         displayContent()
