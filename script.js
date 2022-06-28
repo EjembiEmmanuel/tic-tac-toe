9cbee50f6bc257084134db0559a659f51ef8b446
Ejembi Emmanuel <ejembioche50@gmail.com>
2022-06-28 12:09:40 +0100

    Created functions that handles game over events

--- script.js
+++ script.js
@@ -41,7 +41,9 @@ const GetPlayerTurn = (player1, player2) => {
 
 const Gameboard = (function() {
     let gameboard = [
-            
+        "", "", "",
+        "", "", "",
+        "", "", "",
     ]
 
     return {
@@ -64,6 +66,127 @@ const DisplayController = (function(doc, board) {
         return board.gameboard
     }
 
+
+    const checkForThree = (a, b, c) => {
+        let isWon 
+
+        if(a === b && b === c) {
+            isWon = true
+        } else {
+            isWon = false
+        }
+
+        return isWon
+    }
+
+    const gameOver = (content) => {
+        const {player1, player2} = GetPlayer()
+
+        content = getContent()
+        
+        let outcome
+        
+        if(content.length > 1) {
+
+            if(content[0] !== "" && content[1] !== "" && content[2] !== "") {
+                outcome = checkForThree(content[0], content[1], content[2])
+
+                if(outcome) {
+                    if(player1.letter == content[0]) {
+                        console.log(`${player1.name} wins`)
+                    } else {
+                        console.log(`${player2.name} wins`)
+                    }
+                }
+            } 
+            
+            if(content[3] !== "" && content[4] !== "" && content[5] !== "") {
+                outcome = checkForThree(content[3], content[4], content[5])
+
+                if(outcome) {
+                    if(player1.letter == content[3]) {
+                        console.log(`${player1.name} wins`)
+                    } else {
+                        console.log(`${player2.name} wins`)
+                    }
+                }
+            } 
+
+            if(content[6] !== "" && content[7] !== "" && content[8] !== "") {
+                outcome = checkForThree(content[6], content[7], content[8])
+
+                if(outcome) {
+                    if(player1.letter == content[6]) {
+                        console.log(`${player1.name} wins`)
+                    } else {
+                        console.log(`${player2.name} wins`)
+                    }
+                }
+            } 
+            
+            if(content[0] !== "" && content[3] !== "" && content[6] !== "") {
+                outcome = checkForThree(content[0], content[3], content[6])
+
+                if(outcome) {
+                    if(player1.letter == content[0]) {
+                        console.log(`${player1.name} wins`)
+                    } else {
+                        console.log(`${player2.name} wins`)
+                    }
+                }
+            } 
+            
+            if(content[1] !== "" && content[4] !== "" && content[7] !== "") {
+                outcome = checkForThree(content[1], content[4], content[7])
+
+                if(outcome) {
+                    if(player1.letter == content[1]) {
+                        console.log(`${player1.name} wins`)
+                    } else {
+                        console.log(`${player2.name} wins`)
+                    }
+                    
+                }
+            } 
+            
+            if(content[2] !== "" && content[5] !== "" && content[8] !== "") {
+                outcome = checkForThree(content[2], content[5], content[8])
+
+                if(outcome) {
+                    if(player1.letter == content[2]) {
+                        console.log(`${player1.name} wins`)
+                    } else {
+                        console.log(`${player2.name} wins`)
+                    }
+                }
+            } 
+            
+            if(content[0] !== "" && content[4] !== "" && content[8] !== "") {
+                outcome = checkForThree(content[0], content[4], content[8])
+
+                if(outcome) {
+                    if(player1.letter == content[0]) {
+                        console.log(`${player1.name} wins`)
+                    } else {
+                        console.log(`${player2.name} wins`)
+                    }
+                }
+            } 
+            
+            if(content[2] !== "" && content[4] !== "" && content[6] !== "") {
+                outcome = checkForThree(content[2], content[4], content[6])
+
+                if(outcome) {
+                    if(player1.letter == content[2]) {
+                        console.log(`${player1.name} wins`)
+                    } else {
+                        console.log(`${player2.name} wins`)
+                    }
+                }
+            }
+        }
+    }
+
     const displayContent = (selectors, content) => {
     
         const {player1, player2} = GetPlayer()
@@ -75,11 +198,10 @@ const DisplayController = (function(doc, board) {
             const index = Array.from(selectors).indexOf(btn);
 
             let player = GetPlayerTurn(player1, player2)
-            console.log(player)
 
             if(btn.innerText === "") {
                 btn.innerText = player.letter
-                content.push(btn.innerText)
+                content[index] = player.letter
 
                 gameOver()
             }
@@ -87,7 +209,6 @@ const DisplayController = (function(doc, board) {
     }
 
     
-
     const displayBoard = () => {
         displayContent()
     }
