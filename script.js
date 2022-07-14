8465d2ad20add41c4ff77a331c1bd9f1c39dbfe5
Emmanuel Ejembi <ejembioche50@gmail.com>
2022-07-14 04:47:24 +0100

    Added features that:     - Checks for winner     - Displays winning message     - Resets the game

--- script.js
+++ script.js
@@ -11,12 +11,34 @@ const GameBoard = (function() {
 
 }())
 
+const WinningCombos = (function() {
+    const combos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
 
-const MakeMove = (function(doc, gameBoard) {
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+
+    return {
+        combos
+    }
+}())
+
+
+const MakeMove = (function(doc, gameBoard, winning) {
     const PLAYER_X = 'x'
     const PLAYER_O = 'circle'
     const cellElements = doc.querySelectorAll('[data-cell]')
     const board = doc.getElementById('board')
+    const winningMessageElement = doc.getElementById('winning-message')
+    const winningMessageTextElement = doc.querySelector('[data-winning-message-text]')
+    const restartButton = doc.getElementById('restart-button')
     let circleTurn
 
     const placeMarker = (cell, currentPlayer) => {
@@ -46,21 +68,56 @@ const MakeMove = (function(doc, gameBoard) {
         const currentPlayer = circleTurn ? PLAYER_O : PLAYER_X
         placeMarker(cell, currentPlayer)
 
-        console.log(gameBoard.gameboard)
+        if(checkWinner(currentPlayer)) {
+            endGame(false)
+        } else if(isDraw()) {
+            endGame(true)
+        } else {
+            swapTurns()
+            setBoardHoverClass()
+        }
+    }
 
-        swapTurns()
-        setBoardHoverClass()
+    const checkWinner = (currentPlayer) => {
+        return winning.combos.some(combination => {
+            return combination.every(index => {
+                return cellElements[index].classList.contains(currentPlayer)
+            })
+        })
+    }
+
+    const endGame = (draw) => {
+        if(draw) {
+            winningMessageTextElement.innerText = "Draw!"
+        } else {
+            winningMessageTextElement.innerText = `${circleTurn ? "O" : "X"} Wins!`
+        }
+
+        winningMessageElement.classList.add('show')
+    }
+
+    const isDraw = () => {
+        return [...cellElements].every(cell => {
+            return cell.classList.contains(PLAYER_X) || cell.classList.contains(PLAYER_O)
+        })
     }
 
     const startGame = () => {
         circleTurn = false
 
         cellElements.forEach(cell => {
+            cell.classList.remove(PLAYER_X)
+            cell.classList.remove(PLAYER_O)
+            cell.removeEventListener('click', clickHandler)
             cell.addEventListener('click', clickHandler, { once: true })
         })
 
         setBoardHoverClass()
+        winningMessageElement.classList.remove('show')
+
     }
 
     startGame()
-}(document, GameBoard))
+
+    restartButton.addEventListener('click', startGame)
+}(document, GameBoard, WinningCombos))
